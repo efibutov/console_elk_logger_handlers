62a9d2163b4431992fd5cfc8a0fc48eefa067479
Efi Butovski <efibutov@gmail.com>
2026-04-01 17:53:49 +0300

    Removed unneeded requirements

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
         'elastic-transport==9.2.1',
         'elasticsearch==9.3.0',
         'python-dateutil==2.9.0.post0',
-        'requests==2.33.1',
         'termcolor==3.3.0',
     ],
     author='Efi Butovski',
