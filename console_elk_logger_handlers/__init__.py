ee589b064b031da6c50bac0625b77f166ab3049b
Efi Butovski <efibutov@gmail.com>
2026-04-01 22:29:54 +0300

    Lots of changes

--- console_elk_logger_handlers/__init__.py
+++ console_elk_logger_handlers/__init__.py
@@ -1,4 +1,4 @@
-from console_elk_logger_handlers.colorized_console import ColorizedConsole
-from console_elk_logger_handlers.elk_handler import ELKHandler
+from handlers.colorized_console import ColorizedConsole
+from handlers.elk_handler import ELKHandler
 
 __all__ = ['ELKHandler', 'ColorizedConsole']
