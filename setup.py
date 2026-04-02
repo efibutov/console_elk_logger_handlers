a8f7b54a15d5cb88bc7aec3f92896a18c4d75eeb
Efi Butovski <efibutov@gmail.com>
2026-04-02 03:09:38 +0300

    pyproject.toml modified

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='console_elk_logger_handlers',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
     install_requires=[
         'elastic-transport',
         'elasticsearch',
@@ -11,6 +12,7 @@ setup(
         'termcolor',
     ],
     author='Efi Butovski',
+    author_email='efibutov@gmail.com',
     description='Simple colored console and ELK logger for Python',
     python_requires='>=3.11',
 )
