4045d7e75750ad192701c50eeb0583d70a2c0bd2
Efi Butovski <efibutov@gmail.com>
2026-04-02 03:19:40 +0300

    pyproject.toml modified

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='console_elk_logger_handlers',
     version='0.1.0',
-    packages=find_packages(exclude=['tests*']),
+    packages=find_packages('console_elk_logger_handlers'),
     license='MIT',
     install_requires=[
         'elastic-transport',
