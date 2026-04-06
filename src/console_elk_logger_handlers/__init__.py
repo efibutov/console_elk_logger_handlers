23a8f7ad522e81ad550d9abadc2982a20f5a334a
Efi Butovski <efibutov@gmail.com>
2026-04-07 00:55:41 +0300

    Default logger generator added

--- src/console_elk_logger_handlers/__init__.py
+++ src/console_elk_logger_handlers/__init__.py
@@ -1,4 +1,27 @@
-# from console_elk_logger_handlers.colorized_console import ColorizedConsole
-# from console_elk_logger_handlers.elk_handler import ELKHandler
+from logging import getLogger, DEBUG
+from .colorized_console import ColorizedConsole
+from .elk_handler import ELKHandler
 
-# __all__ = ['ELKHandler', 'ColorizedConsole']
+
+def get_common_loggers(
+    name,
+    elk_index='probe',
+    elk_host='host.docker.internal',
+    elk_port=9200,
+    password='changeme',
+    default_level=DEBUG
+):
+    LOGGER = getLogger(name=name)
+    LOGGER.addHandler(ColorizedConsole(name=name))
+    LOGGER.setLevel(default_level)
+    LOGGER.addHandler(
+        ELKHandler(
+            index=elk_index,
+            host=elk_host,
+            port=elk_port,
+            password=password
+        )
+    )
+    return LOGGER
+
+__all__ = ['ELKHandler', 'ColorizedConsole', 'get_common_loggers']
